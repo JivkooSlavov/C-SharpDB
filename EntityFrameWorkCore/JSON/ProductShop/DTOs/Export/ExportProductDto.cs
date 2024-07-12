00ac364d451765e35c7fa29a82a7f300da67be5d
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-07-12 16:37:36 +0300

    Add files via upload

---
+++ EntityFrameWorkCore/JSON/ProductShop/DTOs/Export/ExportProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductShop.DTOs.Export
+{
+    public class ExportProductDto
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Seller { get; set; } 
+
+    }
+}
