39ed6b57db87cba37aba5c8bd978b4e7ce8348c5
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-07-16 17:08:23 +0300

    Add files via upload

---
+++ EntityFrameWorkCore/LINQ/MusicHub/Data/ValidationConstrants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data
+{
+    public class ValidationConstrants
+    {
+        public const int NameOfSong = 20;
+
+        public const int NameOfAlbum = 40;
+
+        public const int NameOfPerformer = 20;
+
+        public const int NameOfProducer = 30;
+
+        public const int NameOfWriter = 20;
+    }
+}
