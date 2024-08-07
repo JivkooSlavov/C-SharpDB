1c52c86ead61344b3b4c42c61f566c815eb7bb64
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-08-07 22:02:00 +0300

    Add files via upload

---
+++ EntityFrameWorkCore/ExamPreparation/Cadastre/Cadastre/DataProcessor/ImportDtos/ImportDistrictsXmlDto.cs
@@ -0,0 +1,36 @@
+﻿using Cadastre.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static Cadastre.Data.Constrain;
+
+namespace Cadastre.DataProcessor.ImportDtos
+{
+    [XmlType(nameof(District))]
+    public class ImportDistrictsXmlDto
+    {
+        [XmlElement(nameof(Name))]
+
+        [Required]
+        [MinLength(DistrictNameMinLength)]
+        [MaxLength(DistrictNameMaxLength)]
+        public string Name { get; set; }
+
+        [XmlElement(nameof(PostalCode))]
+        [Required]
+        [RegularExpression(@"^[A-Z]{2}-\d{5}$")]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [XmlAttribute(nameof(Region))]
+        public string Region {  get; set; }
+
+
+        [XmlArray(nameof(Properties))]
+        public ImportPropertiesDtoXml[] Properties {  get; set; }
+    }
+}
