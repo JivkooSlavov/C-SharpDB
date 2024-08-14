fcfc6bf8d9c03a0c4e16b9b733037fb18e28cb2b
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-08-14 20:56:12 +0300

    Add files via upload

---
+++ EntityFrameWorkCore/ExamPreparation/Cadastre/Cadastre/Data/Models/Property.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Cadastre.Data.Constrain;
+
+namespace Cadastre.Data.Models
+{
+    public class Property
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(PropertyIndetifierMaxLength)]
+        public string PropertyIdentifier {get; set; }
+
+        [Required]
+        public int Area { get; set; }
+
+        [MaxLength(PropertyDetailsMaxLength)]
+        public string Details { get; set; }
+        [Required]
+        [MaxLength(PropertyAddressMaxLength)]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime DateOfAcquisition { get; set; }
+        
+        [Required]
+        [ForeignKey(nameof(DistrictId))]
+        public int DistrictId { get; set; }
+
+        public virtual District District {get; set; }
+
+        public virtual ICollection<PropertyCitizen> PropertiesCitizens { get; set; } = new HashSet<PropertyCitizen>();
+    }
+}
---
+++ EntityFrameWorkCore/ExamPreparation/Cadastre/Cadastre/DataProcessor/ExportDtos/ExportOwnersJsonDtop.cs
@@ -0,0 +1,16 @@
+﻿using Cadastre.Data.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastre.DataProcessor.ExportDtos
+{
+    public class ExportOwnersJsonDtop
+    {
+        public string LastName { get; set; }
+        public string MaritalStatus { get; set; }
+    }
+}
