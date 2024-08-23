136feacf7b96294cb0333ddcb3371376c9e590c0
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-08-23 21:23:26 +0300

    Delete EntityFrameWorkCore/ExercisesEntityRelations/FootballBetting directory

--- EntityFrameWorkCore/ExercisesEntityRelations/FootballBetting/P02_FootballBetting.Common/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P02_FootballBetting.Common
-{
-    public static class Config
-    {
-        public const string ConnectionString = "Server=.;Database=FootballBookmakerSystem;Integrated Security=True;";
-    }
-}
--- EntityFrameWorkCore/ExercisesEntityRelations/FootballBetting/P02_FootballBetting.Data.Models/Enum/Prediction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace P02_FootballBetting.Data.Models.Enum
-{
-    public enum Prediction
-    {
-        Win= 0,
-        Lose = 1,
-        Draw = 2
-    }
-}
