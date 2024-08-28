3c585290f117c205a393bf30fd5482c2144d01e8
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-08-28 21:22:00 +0300

    Delete EntityFrameWorkCore/ExercisesEntityRelations/HospitalDatabase directory

--- EntityFrameWorkCore/ExercisesEntityRelations/HospitalDatabase/P01_HospitalDatabase.Data.Common/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P01_HospitalDatabase.Data.Common
-{
-    public static class Config
-    {
-        public const string ConnectionString = "Server=.;Database=HospitalDatabase;Integrated Security=True;";
-    }
-
-}
