a191890749da7421b7e367ffa57a35bf88245abd
JivkooSlavov <146717793+JivkooSlavov@users.noreply.github.com>
2024-08-28 21:22:45 +0300

    Add files via upload

---
+++ EntityFrameWorkCore/ExercisesEntityRelations/HospitalDatabase/P01_HospitalDatabase.Data.Common/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_HospitalDatabase.Data.Common
+{
+    public static class Config
+    {
+        public const string ConnectionString = "Server=.;Database=HospitalDatabase;Integrated Security=True;";
+    }
+
+}
